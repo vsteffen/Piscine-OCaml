25b1632baab7eefb9531643cb743bcb0fb7566af
vsteffen <vsteffen@student.42.fr>
2019-05-21 18:18:40 +0200

    Modify ft_sum to accept negative index

--- d01/ex08/ft_sum.ml
+++ d01/ex08/ft_sum.ml
@@ -5,7 +5,7 @@ let ft_sum fn iLower iUpper =
 		else
 			loop ( i + 1 ) (acc +. (fn i))
 	in
-	if iLower > iUpper || iLower < 0
+	if iLower > iUpper
 		then nan
 	else
 		loop iLower 0.
@@ -16,7 +16,7 @@ let main () =
 	print_float ( ft_sum (fun i -> float_of_int (i * i)) 0 1 ) ; print_char '\n' ;
 	print_float ( ft_sum (fun i -> float_of_int (i * i)) 0 2 ) ; print_char '\n' ;
 	print_float ( ft_sum (fun i -> float_of_int (i * i)) 1 0 ) ; print_char '\n' ;
-	print_float ( ft_sum (fun i -> float_of_int (i * i)) (-1) 10 ) ; print_char '\n' ;
+	print_float ( ft_sum (fun i -> float_of_int (i * i)) (-1) 2 ) ; print_char '\n' ;
 	print_float ( ft_sum (fun i -> float_of_int (i * i)) 1 100000000 ) ; print_char '\n'
 
 let () = main ()
