35a7f132433aa7135ba308e81894a8ac4486ac2c
vsteffen <vsteffen@student.42.fr>
2019-05-29 11:15:57 -0400

    Add ex04 in d06

---
+++ d06/ex04/ex04.ml
@@ -0,0 +1,63 @@
+module type VAL =
+sig
+	type t
+	val add : t -> t -> t
+	val mul : t -> t -> t
+end
+
+module type EVALEXPR =
+sig
+	type t
+	type expr = Add of expr * expr | Mul of expr * expr | Value of t
+	val eval : expr -> t
+end
+
+module type MAKEEVALEXPR =
+	functor (EvalExpr : VAL) ->
+		EVALEXPR with type t = EvalExpr.t
+
+module MakeEvalExpr : MAKEEVALEXPR =
+	functor (EvalExpr : VAL) ->
+		struct
+			type t = EvalExpr.t
+			type expr = Add of expr * expr | Mul of expr * expr | Value of t
+			let rec eval = function
+				| Add (x, y) -> EvalExpr.add (eval x) (eval y)
+				| Mul (x, y) -> EvalExpr.mul (eval x) (eval y)
+				| Value (x)  -> x
+		end
+
+module IntVal : (VAL with type t = int) =
+struct
+	type t = int
+	let add = ( + )
+	let mul = ( * )
+end
+
+module FloatVal : (VAL with type t = float) =
+struct
+	type t = float
+	let add = ( +. )
+	let mul = ( *. )
+end
+
+module StringVal : (VAL with type t = string) =
+struct
+	type t = string
+	let add s1 s2 = if (String.length s1) > (String.length s2) then s1 else s2
+	let mul = ( ^ )
+end
+
+module IntEvalExpr		: (EVALEXPR with type t := IntVal.t) = MakeEvalExpr (IntVal)
+module FloatEvalExpr	: (EVALEXPR with type t := FloatVal.t) = MakeEvalExpr (FloatVal)
+module StringEvalExpr	: (EVALEXPR with type t := StringVal.t) = MakeEvalExpr (StringVal)
+
+let ie = IntEvalExpr.Add (IntEvalExpr.Value 40, IntEvalExpr.Value 2)
+let fe = FloatEvalExpr.Add (FloatEvalExpr.Value 41.5, FloatEvalExpr.Value 0.92)
+let se = StringEvalExpr.Mul (StringEvalExpr.Value "very ",
+	(StringEvalExpr.Add (StringEvalExpr.Value "very long",
+	StringEvalExpr.Value "short")))
+
+let () = Printf.printf "Res = %d\n" (IntEvalExpr.eval ie)
+let () = Printf.printf "Res = %f\n" (FloatEvalExpr.eval fe)
+let () = Printf.printf "Res = %s\n" (StringEvalExpr.eval se)
